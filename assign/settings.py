1de90bdca5ffd02a4c416710fde4b16cd402e669
Ramakant <jogi.ramakant94@gmail.com>
2024-08-05 19:44:01 +0530

    feat: configure MySQL database

--- assign/settings.py
+++ assign/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'assign.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'api_assignment',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root',
     }
 }
 
